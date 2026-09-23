e1f331228fed2f7ad0d52e34fd461aafaaacae17
Cristian M <cpmelgoza05@gmail.com>
2026-09-22 20:32:43 -0700

    Edit Project physics settings
    Increase physics calculations steps for more accuracy. Also decreased bounce threshold to remove scenarios where balls would start sliding on walls of floors when bouncing at a glancing angle

--- Assets/Scripts/BallBody.cs
+++ Assets/Scripts/BallBody.cs
@@ -22,12 +22,6 @@ public class BallBody : MonoBehaviour
 
     private void Update()
     {
-        DebugPanel.Instance.SetDebugLabel(0, "KE", GetKineticEnergy().ToString("F2"));
-        DebugPanel.Instance.SetDebugLabel(1, "PE", GetPotentialEnergy().ToString("F2"));
-        DebugPanel.Instance.SetDebugLabel(2, "TE", GetTotalEnergy().ToString("F2"));
+        
     }
-
-    private float GetKineticEnergy() => 0.5f*rb.mass*rb.linearVelocity.sqrMagnitude;
-    private float GetPotentialEnergy() => rb.mass*(Physics2D.gravity.magnitude*rb.gravityScale)*(rb.position.y - floorHeight);
-    private float GetTotalEnergy() => GetKineticEnergy() + GetPotentialEnergy();
 }
